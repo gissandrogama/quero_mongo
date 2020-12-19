ceb24a0d7f2d8c1147e74b449fee63edcbf620f1
gissandrogama <gissandrogama@gmail.com>
2020-12-19 15:27:56 -0300

    formating

--- lib/queromongo_api_web/controllers/courses_controller.ex
+++ lib/queromongo_api_web/controllers/courses_controller.ex
@@ -2,9 +2,10 @@ defmodule QueromongoApiWeb.CoursesController do
   use QueromongoApiWeb, :controller
 
   def index(conn, params) do
-    response = Mongo.find(:mongo, "courses", params)
-    |> Enum.to_list()
-    |> Enum.map(&Map.delete(&1, "_id"))
+    response =
+      Mongo.find(:mongo, "courses", params)
+      |> Enum.to_list()
+      |> Enum.map(&Map.delete(&1, "_id"))
 
     conn
     |> json(response)
